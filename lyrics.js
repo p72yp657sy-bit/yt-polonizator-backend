a967f57c13b36f4628f9c93af2085e9d97cee2f7
p72yp657sy-bit <p72yp657sy@privaterelay.appleid.com>
2026-09-24 13:36:21 +0200

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     // Konfiguracja nagłówków CORS dla bezpieczeństwa i dostępu z frontendu
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
