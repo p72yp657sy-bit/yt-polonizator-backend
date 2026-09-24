1225f88e382b9eb645cb6f332baa4dd7fa88ecdd
p72yp657sy-bit <p72yp657sy@privaterelay.appleid.com>
2026-09-24 15:58:41 +0200

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,13 @@
+{
+  "version": 2,
+  "routes": [
+    {
+      "src": "/api/captions",
+      "dest": "/api/captions.js"
+    },
+    {
+      "src": "/api/lyrics",
+      "dest": "/api/lyrics.js"
+    }
+  ]
+}
